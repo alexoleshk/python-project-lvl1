49d20877953b0bd19f87b31cdbf222e92b2bc4ac
alexoleshk <ajiekc42@gmail.com>
2021-06-15 21:17:22 +0300

    add 'gcd' key to helpers

--- brain_games/helpers.py
+++ brain_games/helpers.py
@@ -1,5 +1,6 @@
 from brain_games.games.calc import get_quest_ans_calc
 from brain_games.games.even import get_quest_ans_even
+from brain_games.games.gcd import get_quest_ans_gcd
 from brain_games.games.progression import get_quest_ans_progr
 
 
@@ -8,6 +9,7 @@ def get_task(game):
         'even': 'Answer "yes" if the number is even, otherwise answer "no".',
         'progression': 'What number is missing in the progression?',
         'calc': 'What is the result of the expression?',
+        'gcd': 'Find the greatest common divisor of given numbers.',
     }
     return tasks[game]
 
@@ -17,5 +19,6 @@ def get_question_answer(game):
         'even': get_quest_ans_even,
         'progression': get_quest_ans_progr,
         'calc': get_quest_ans_calc,
+        'gcd': get_quest_ans_gcd
     }
     return funcs[game]()
