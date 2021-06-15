73d32bea1297da38aa9a69c492587488a26812a6
alexoleshk <ajiekc42@gmail.com>
2021-06-15 16:17:54 +0300

    add 'calc' key to helpers

--- brain_games/helpers.py
+++ brain_games/helpers.py
@@ -1,3 +1,4 @@
+from brain_games.games.calc import get_quest_ans_calc
 from brain_games.games.even import get_quest_ans_even
 from brain_games.games.progression import get_quest_ans_progr
 
@@ -6,6 +7,7 @@ def get_task(game):
     tasks = {
         'even': 'Answer "yes" if the number is even, otherwise answer "no".',
         'progression': 'What number is missing in the progression?',
+        'calc': 'What is the result of the expression?',
     }
     return tasks[game]
 
@@ -14,5 +16,6 @@ def get_question_answer(game):
     funcs = {
         'even': get_quest_ans_even,
         'progression': get_quest_ans_progr,
+        'calc': get_quest_ans_calc,
     }
     return funcs[game]()
