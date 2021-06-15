c5f34c85cd651ba4d9be9e215b6f2184b888bd3d
alexoleshk <ajiekc42@gmail.com>
2021-06-15 16:05:54 +0300

    add 'progression' key to helpers

--- brain_games/helpers.py
+++ brain_games/helpers.py
@@ -1,15 +1,18 @@
 from brain_games.games.even import get_quest_ans_even
+from brain_games.games.progression import get_quest_ans_progr
 
 
 def get_task(game):
     tasks = {
         'even': 'Answer "yes" if the number is even, otherwise answer "no".',
+        'progression': 'What number is missing in the progression?',
     }
     return tasks[game]
 
 
 def get_question_answer(game):
     funcs = {
-        'even': get_quest_ans_even
+        'even': get_quest_ans_even,
+        'progression': get_quest_ans_progr,
     }
     return funcs[game]()
