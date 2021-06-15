09ff18ea51036d47bf592cb03cf377b832200a0c
alexoleshk <ajiekc42@gmail.com>
2021-06-15 16:19:01 +0300

    refactor brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-from brain_games.common_funcs import greeting
-from brain_games.games.gcd import gcd_game
+from brain_games.engine import engine
 
 
 def main():
-    name = greeting()
-    if gcd_game(name):
-        print('Congratulations, {}!'.format(name))
+    engine('gcd')
 
 
 if __name__ == '__main__':
