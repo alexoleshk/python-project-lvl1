2db370cf257eae73d5996c55a21605f0525ab494
alexoleshk <ajiekc42@gmail.com>
2021-06-15 16:14:01 +0300

    add get_quest_ans_calc() to calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -19,3 +19,14 @@ def calc_game(name):
         if not check_answer(name, answer, right_answer):
             return False
     return True
+
+
+def get_quest_ans_calc():
+    max_num = 25
+    a = random.randint(1, max_num)
+    b = random.randint(1, max_num)
+    ops = {'+': operator.add, '-': operator.sub, '*': operator.mul}
+    op = random.choice(list(ops.keys()))
+    question = 'Question: {} {} {}'.format(a, op, b)
+    correct_answer = ops[op](a, b)
+    return question, correct_answer
