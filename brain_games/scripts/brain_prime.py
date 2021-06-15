d885f37ec5ab1f56e33e26b12cb0b1dd189fdede
alexoleshk <ajiekc42@gmail.com>
2021-06-15 21:20:12 +0300

    refactor brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-from brain_games.common_funcs import greeting
-from brain_games.games.prime import prime_game
+from brain_games.engine import engine
 
 
 def main():
-    name = greeting()
-    if prime_game(name):
-        print('Congratulations, {}!'.format(name))
+    engine('prime')
 
 
 if __name__ == '__main__':
