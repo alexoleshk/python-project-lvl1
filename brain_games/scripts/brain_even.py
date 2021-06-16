0c7bbbdd530e4247d86bb52bc19f65156622499e
alexoleshk <ajiekc42@gmail.com>
2021-06-16 17:43:13 +0300

    refactor brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+from brain_games.games.even import get_quest_ans_even
 from brain_games.engine import engine
 
 
 def main():
-    engine('even')
+    task = 'Answer "yes" if the number is even, otherwise answer "no".'
+    engine(task, get_quest_ans_even)
 
 
 if __name__ == '__main__':
