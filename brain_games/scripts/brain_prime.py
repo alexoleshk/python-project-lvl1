eeacdbd6b6b95bf64bd71eff7ced76a342733121
alexoleshk <ajiekc42@gmail.com>
2021-06-16 17:50:29 +0300

    refactor brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+from brain_games.games.prime import get_quest_ans_prime
 from brain_games.engine import engine
 
 
 def main():
-    engine('prime')
+    task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    engine(task, get_quest_ans_prime)
 
 
 if __name__ == '__main__':
