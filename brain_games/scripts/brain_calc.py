eb3ac30f900ba5ccc06eb7478b036c6a1335f888
alexoleshk <ajiekc42@gmail.com>
2021-06-16 17:46:31 +0300

    refactor brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+from brain_games.games.calc import get_quest_ans_calc
 from brain_games.engine import engine
 
 
 def main():
-    engine('calc')
+    task = 'What is the result of the expression?'
+    engine(task, get_quest_ans_calc)
 
 
 if __name__ == '__main__':
