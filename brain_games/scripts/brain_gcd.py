e81f97d16ce294bf0468d89c2a99615b01fd4d98
alexoleshk <ajiekc42@gmail.com>
2021-06-16 17:48:31 +0300

    refactor brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+from brain_games.games.gcd import get_quest_ans_gcd
 from brain_games.engine import engine
 
 
 def main():
-    engine('gcd')
+    task = 'Find the greatest common divisor of given numbers.'
+    engine(task, get_quest_ans_gcd)
 
 
 if __name__ == '__main__':
