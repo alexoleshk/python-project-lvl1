06ea82442a8bc1b54f126ce6d7d5579e2bb4ae5c
alexoleshk <ajiekc42@gmail.com>
2021-06-18 18:00:45 +0300

    add is_even() to even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,8 +3,12 @@ TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 MAX_NUM = 100
 
 
+def is_even(n):
+    return False if n % 2 else True
+
+
 def get_question_answer():
     n = random.randint(1, MAX_NUM)
     question = 'Question: {}'.format(n)
-    correct_answer = 'no' if n % 2 else 'yes'
+    correct_answer = 'yes' if is_even(n) else 'no'
     return question, correct_answer
