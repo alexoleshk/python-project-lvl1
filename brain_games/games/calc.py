f6661ea877e7a1b2209f8a3c96e055d80698f5e9
alexoleshk <ajiekc42@gmail.com>
2021-06-18 18:58:30 +0300

    add calculate_result() to calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,11 +4,16 @@ TASK = 'What is the result of the expression?'
 MAX_NUM = 25
 
 
+def calculate_result(a, b, op, ops):
+    return ops[op](a, b)
+
+
 def get_question_answer():
     a = random.randint(1, MAX_NUM)
     b = random.randint(1, MAX_NUM)
     ops = {'+': operator.add, '-': operator.sub, '*': operator.mul}
     op = random.choice(list(ops.keys()))
     question = 'Question: {} {} {}'.format(a, op, b)
-    correct_answer = str(ops[op](a, b))
+    result = calculate_result(a, b, op, ops)
+    correct_answer = str(result)
     return question, correct_answer
