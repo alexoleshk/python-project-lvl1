37c359e6367c15800e619e7518abeb515ca0f534
alexoleshk <ajiekc42@gmail.com>
2021-06-19 22:08:55 +0300

    refactor progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,19 +1,19 @@
 import random
+
 TASK = 'What number is missing in the progression?'
 PROGR_LEN = 10
 MAX_FIRST_ELEM = 42
 MAX_STEP = 9
 
 
-def generate_progression():
-    step = random.randint(1, MAX_STEP)
-    first_elem = random.randint(1, MAX_FIRST_ELEM)
-    return list(range(first_elem,
-                      first_elem + step * (PROGR_LEN - 1) + 1, step))
+def generate_progression(first_elem, step):
+    return list(range(first_elem, first_elem + step * PROGR_LEN, step))
 
 
 def get_question_answer():
-    progression = generate_progression()
+    first_elem = random.randint(1, MAX_FIRST_ELEM)
+    step = random.randint(1, MAX_STEP)
+    progression = generate_progression(first_elem, step)
     index_to_hide = random.randint(0, PROGR_LEN - 1)
     correct_answer = str(progression[index_to_hide])
     progression[index_to_hide] = '..'
