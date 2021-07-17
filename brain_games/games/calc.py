a7d8c86cdafa9585cec8e547edb3ad4dc5f867ba
Alexander Oleshkevich <ajiekc42@gmail.com>
2021-07-17 15:18:33 +0300

    refactor calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,17 +3,17 @@ import operator
 
 TASK = 'What is the result of the expression?'
 MAX_NUM = 25
+OPS = {'+': operator.add, '-': operator.sub, '*': operator.mul}
 
 
 def calculate(operand_one, operand_two, operation):
-    ops = {'+': operator.add, '-': operator.sub, '*': operator.mul}
-    return ops[operation](operand_one, operand_two)
+    return OPS[operation](operand_one, operand_two)
 
 
 def get_question_answer():
     operand_two = random.randint(1, MAX_NUM)
     operand_one = random.randint(1, MAX_NUM)
-    operation = random.choice(['+', '-', '*'])
+    operation = random.choice(list(OPS.keys()))
     question = '{} {} {}'.format(operand_one, operation, operand_two)
     result = calculate(operand_one, operand_two, operation)
     correct_answer = str(result)
